ab01eab4bd1aa4f9d01710f212c2af05e2522ab9
mmwebster <milo.webster@gmail.com>
2016-07-21 22:13:17 -0700

    Major infrastructure changes - change log(ish) in google drive document for 7/21/2016

--- config/deploy.js
+++ config/deploy.js
@@ -27,18 +27,18 @@ module.exports = function(deployTarget) {
 
   if (deployTarget === 'qa' || deployTarget === 'prod') {
     ENV.build.environment = 'production';
-    ENV.s3.accessKeyId = process.env.AWS_KEY;
-    ENV.s3.secretAccessKey = process.env.AWS_SECRET;
-    ENV.s3.bucket = /* YOUR S3 BUCKET NAME */;
-    ENV.s3.region = /* YOUR S3 REGION */;
+    ENV.s3.accessKeyId = "AKIAJ2NOQ4ICZQRCBHPA"; // process.env.AWS_KEY;
+    ENV.s3.secretAccessKey = "kDGF+UxHUzdVTa5rDPEVz3xoyQ7mZKXemelCYMIp"; // process.env.AWS_SECRET;
+    ENV.s3.bucket = "reqourse-assets"; /* YOUR S3 BUCKET NAME */
+    ENV.s3.region = "us-west-1"; /* YOUR S3 REGION */
   }
 
   if (deployTarget === 'qa') {
-    ENV.redis.url = process.env.QA_REDIS_URL;
+    ENV.redis.url = "redis://h:p640e02u5m2311el6upvl9ll78s@ec2-50-16-205-207.compute-1.amazonaws.com:6399"; // process.env.QA_REDIS_URL;
   }
 
   if (deployTarget === 'prod') {
-    ENV.redis.url = process.env.PROD_REDIS_URL;
+    ENV.redis.url = " redis://h:pf8bv4r55ks14l6tn999fbire6n@ec2-174-129-243-209.compute-1.amazonaws.com:30179"; // process.env.PROD_REDIS_URL;
   }
 
   return ENV;
