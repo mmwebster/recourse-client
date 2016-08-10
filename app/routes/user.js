e9321496f94b18369c10c621fe8ccac78fb364b7
mmwebster <milo.webster@gmail.com>
2016-08-09 19:20:42 -0700

    restructured templates and components

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,4 +2,7 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  redirect: function() {
+    this.transitionTo('user.dashboard');
+  }
 });
