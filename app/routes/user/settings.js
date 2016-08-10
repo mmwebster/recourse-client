0f9e0a233886ef9d54e993cdef05d328e28694d2
mmwebster <milo.webster@gmail.com>
2016-08-10 15:17:54 -0700

    improving content structure in templates

--- app/routes/user/settings.js
+++ app/routes/user/settings.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
+const { service } = Ember.inject;
+
 export default Ember.Route.extend({
+  session: service('session'),
+
+  actions: {
+    logout() {
+      this.get('session').invalidate();
+    }
+  }
 });
