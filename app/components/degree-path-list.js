6fd1967dd238836d83a9f6d96d9c92d43441211e
mmwebster <milo.webster@gmail.com>
2016-08-16 16:16:37 -0700

    completed degree-path-list component

--- app/components/degree-path-list.js
+++ app/components/degree-path-list.js
@@ -1,9 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  displayRemoveItemPrompt: false,
+  removePromptTitle: null,
+  removePromptContent: null,
+
+  itemToDelete: null,
+
   actions: {
-    removeItem: function(item) {
-      alert("Removing item " + item.title);
-    }
+    removeItemPrompt: function(item) {
+      // set remove prompt title
+      if (this.get('type') === 'majors') {
+        this.set('removePromptTitle', "Are you sure you want to remove this major?");
+      } else {
+        this.set('removePromptTitle', "Are you sure you want to remove this minor?");
+      }
+      // set remove prompt content
+      this.set('removePromptContent', item.title);
+      this.set('itemToDelete', item);
+      this.set('displayRemoveItemPrompt', true);
+    },
+    closeRemoveItemPrompt: function(response) {
+      // bubble up remove action if user confirms
+      if (response === "remove") {
+        this.get('onDelete')(this.get('itemToDelete'));
+      }
+      // close prompt
+      this.set('displayRemoveItemPrompt', false);
+    },
   },
 });
--- app/controllers/user/onboard/degree-path.js
+++ app/controllers/user/onboard/degree-path.js
@@ -5,7 +5,16 @@ const { isEmpty } = Ember;
 export default Ember.Controller.extend({
   sessionAccount: Ember.inject.service('session-account'),
   school: {
-    degreeMajors: [{title: "Major 1"},{title: "Major 2"}],
-    degreeMinors: [{title: "Minor 1"},{title: "Minor 2"}],
+    degreeMajors: [{id: 1, title: "Major 1"},{id: 2, title: "Major 2"}],
+    degreeMinors: [{id: 3, title: "Minor 1"},{id: 4, title: "Minor 2"}],
   },
+
+  actions: {
+    deleteDegreeMajor: function(degreeMajor) {
+      this.get('school.degreeMajors').removeObject(degreeMajor);
+    },
+    deleteDegreeMinor: function(degreeMinor) {
+      this.get('school.degreeMinors').removeObject(degreeMinor);
+    }
+  }
 });
