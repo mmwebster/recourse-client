a00a21960d4405663a229367898910e6738e5bed
mmwebster <milo.webster@gmail.com>
2016-08-15 19:05:02 -0700

    finished users/confirmation ember-paper base implementation

--- app/controllers/users/confirmation.js
+++ app/controllers/users/confirmation.js
@@ -4,7 +4,7 @@ export default Ember.Controller.extend({
   queryParams: ['confirmation_token'],
   confirmation_token: null,
   ajax: Ember.inject.service(),
-  confirming: Ember.computed('confirmation_token', 'confirmed', function() {
+  isConfirming: Ember.computed('confirmation_token', 'confirmed', function() {
     if(!Ember.isEmpty(this.get('confirmation_token')) && this.get('confirmed') === null) {
       return true;
     } else {
@@ -12,20 +12,37 @@ export default Ember.Controller.extend({
     }
   }),
   confirmed: null,
+  showErrorDialog: false,
 
   confirmAccount: Ember.observer('confirmation_token', 'model', function() {
     var token = this.get('confirmation_token');
     var _this = this;
+    this.set('isConfirming', true);
 
     if (!Ember.isEmpty(token)) {
       // Confirm the account
       this.get('ajax').request('/users/confirmation?confirmation_token=' + token).then(function() {
         // Then transition to the login route
+        _this.set('isConfirming', false);
         _this.set('confirmed', true);
       }).catch(function(error) {
+        _this.set('isConfirming', false);
         _this.set('confirmed', false);
+        _this.set('showErrorDialog', true);
         _this.set('errorMessage', error);
       });
     }
-  })
+  }),
+
+  actions: {
+    resendConfirmationEmail: function() {
+      alert("Implement this feature!!");
+    },
+    closeErrorDialog: function() {
+      this.set('showErrorDialog', false);
+    },
+    closeSuccessDialog: function() {
+      this.transitionToRoute('users.login');
+    }
+  }
 });
