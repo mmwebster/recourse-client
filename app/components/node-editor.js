6898098c6266492f22497ef96c311d6b1497cdb9
mmwebster <milo.webster@gmail.com>
2016-09-01 18:08:22 -0700

    finished recursive templating of node-editor and base implementation in courses, majors, minors

--- app/components/node-editor.js
+++ app/components/node-editor.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  nodeHasChildren: Ember.computed('node.children.length', function() {
+    return (this.get('node.children.length') > 0);
+  }),
+  recurrenceDepth: Ember.computed('parentRecurrenceDepth', function() {
+    return this.get('parentRecurrenceDepth') + 1;
+  }),
 });
