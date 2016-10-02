28df224e4f465152859126226f84d74bf3f1e909
mmwebster <milo.webster@gmail.com>
2016-10-01 20:51:34 -0700

    WIP: infinite scroll

--- app/routes/user/admin/courses.js
+++ app/routes/user/admin/courses.js
@@ -1,13 +1,20 @@
 import Ember from 'ember';
+import InfinityRoute from "ember-infinity/mixins/route";
 
 const { service } = Ember.inject;
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(InfinityRoute, {
   sessionAccount: service(),
 
+  perPageParam: "size",              // instead of "per_page"
+  pageParam: "number",                  // instead of "page"
+  // totalPagesParam: "meta.total",
+
   model() {
-    return this.get('sessionAccount.account.school').then((school) => {
-      return school.get('courses');
-    });
+    // TODO: Make this support any school that the admin is a part of
+    return this.infinityModel("course", { perPage: 50, startingPage: 2 });
+    // return this.get('sessionAccount.account.school').then((school) => {
+    //   return school.get('courses');
+    // });
   }
 });
